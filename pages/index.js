afab4761834918b31873f2dccc2c83b6a27e16b7
danyaffa <leffleryd@gmail.com>
2025-04-07 05:34:28 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to KeyPay</h1>
+      <p>Your room key is now your wallet.</p>
+    </div>
+  )
+}
