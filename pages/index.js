39157c3862f277215de2ecd59a4b7c0e9e4b8404
danyaffa <leffleryd@gmail.com>
2025-04-07 09:05:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import '../styles/globals.css';
-
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to KeyPay</h1>
-      <p>Your room key is now your wallet.</p>
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to KeyPay App</h1>
+      <p>Your room key is your wallet.</p>
     </div>
   );
 }
