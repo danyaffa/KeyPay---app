e3aacaa96c73744a219f1991d3d1864b653bfc44
danyaffa <leffleryd@gmail.com>
2025-04-07 08:57:07 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
+import '../styles/globals.css';
 
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to KeyPay</h1>
+      <p>Your room key is now your wallet.</p>
+    </div>
+  );
+}
