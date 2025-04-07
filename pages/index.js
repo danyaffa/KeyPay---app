8d842d1ceceedf785ec2cf9e11b6e1fbadbc3254
danyaffa <leffleryd@gmail.com>
2025-04-07 09:21:48 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Welcome to KeyPay App</h1>
-      <p>Your room key is your wallet.</p>
-    </div>
-  );
-}
